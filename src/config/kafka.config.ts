24a950477fc4b6b0f5edf0c339322d7617a2a0c6
Owais Dashti <owaisdashti@outlook.com>
2024-11-19 12:22:31 +0100

    fix: added env

--- src/config/kafka.config.ts
+++ src/config/kafka.config.ts
@@ -2,7 +2,7 @@ import { Kafka } from 'kafkajs';
 
 export const kafka = new Kafka({
   clientId: 'restaurant-service',
-  brokers: ['kafka:9092', 'kafka:9093'],
+  brokers: [process.env.KAFKA_BROKER ?? 'kafka:9092', 'kafka:9093'],
 });
 
 export const producer = kafka.producer();
