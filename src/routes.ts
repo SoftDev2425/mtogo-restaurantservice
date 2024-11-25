8a0313920b9afda062f4996af92d72a0a450af3f
Andreas Fritzbøger <andreasfritzboger@live.dk>
2024-11-25 11:21:05 +0100

    Completing merge with master and search-and-browse-menus branch

--- src/routes.ts
+++ src/routes.ts
@@ -15,11 +15,9 @@ function routes(app: Express) {
 
   app.use('/api/restaurants', RestaurantRouter);
 
-
   app.use('/api/search', SearchRouter);
 
   app.use('/api/basket', BasketRouter);
-
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   app.use((err: unknown, req: Request, res: Response, _next: NextFunction) => {
