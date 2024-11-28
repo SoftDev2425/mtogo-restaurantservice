8b95f796f2230bca27ed74737b4ce5d6a1a77c36
Owais Dashti <owaisdashti@outlook.com>
2024-11-28 20:06:11 +0100

    fix: remove unnecessary console logs and whitespace in restaurant controller and service

--- src/services/restaurant.service.ts
+++ src/services/restaurant.service.ts
@@ -382,8 +382,6 @@ async function getMenuById(menuId: string) {
 }
 
 async function getRestaurantDetailsByRestaurantId(restaurantId: string) {
-  console.log(restaurantId);
-
   const restaurantData = await fetch(
     `${process.env.AUTH_SERVICE_URL}/api/restaurants/${restaurantId}`,
   );
@@ -403,8 +401,6 @@ async function getRestaurantDetailsByRestaurantId(restaurantId: string) {
       };
     };
   };
-
-  console.log('WE GOT RESTAURANT DATA', restaurant);
 
   const categories = await getCategoriesByRestaurantId(restaurantId);
 
