2940888b7f5f4949f49bf08a10d5d16f9b7248c0
Owais Dashti <owaisdashti@outlook.com>
2024-11-30 15:29:49 +0100

    fix: update Kafka clientId and consumer groupId to reflect new service naming

--- src/config/kafka.config.ts
+++ src/config/kafka.config.ts
@@ -1,9 +1,11 @@
 import { Kafka } from 'kafkajs';
 
 export const kafka = new Kafka({
-  clientId: 'mtogo',
+  clientId: 'mtogo-restaurantservice',
   brokers: [process.env.KAFKA_BROKER ?? 'kafka:9092', 'kafka:9093'],
 });
 
 export const producer = kafka.producer();
-export const consumer = kafka.consumer({ groupId: 'restaurant-service-group' });
+export const consumer = kafka.consumer({
+  groupId: 'mtogo-restaurantservice-group',
+});
